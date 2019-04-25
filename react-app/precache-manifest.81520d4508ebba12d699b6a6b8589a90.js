de19b4d488f02dde6f60cf5eac4de3388b5e8968
cat ChrisTrees <cat@catmini.cat9.private>
2019-04-25 16:07:42 -0500

    react-app v0.0.3-Release Deploy

---
+++ react-app/precache-manifest.81520d4508ebba12d699b6a6b8589a90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "4a085bc89ade4d8fd973",
+    "url": "/react-app/static/js/main.2e58f847.chunk.js"
+  },
+  {
+    "revision": "5f0d6a8aceed172829a6",
+    "url": "/react-app/static/js/2.9ed0dc8e.chunk.js"
+  },
+  {
+    "revision": "4a085bc89ade4d8fd973",
+    "url": "/react-app/static/css/main.4e462cd0.chunk.css"
+  },
+  {
+    "revision": "b65bb5e51336ee479a5ef4cbb7b11ef6",
+    "url": "/react-app/index.html"
+  }
+];
